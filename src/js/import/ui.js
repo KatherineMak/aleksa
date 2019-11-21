049fc7bab2723982907f0716fe2577e3d3815861
KatherineMak <30404996+KatherineMak@users.noreply.github.com>
2019-11-21 18:10:48 +0200

    header

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -12,3 +12,31 @@ $('[rel="modal:open"]').on('click', function(event) {
   });
   return false;
 });
+
+window.onscroll = function showHeaderFixed() {
+  const header = $('.header').get(0);
+  if(window.pageYOffset > 500) {
+      header.classList.add("header-fixed");
+  } else {
+      header.classList.remove("header-fixed");
+  }
+};
+
+$(function() {
+    $(".menu-toggle").click(function(){
+        $(".menu").toggleClass("is-active");
+        $( ".menu-bg" ).fadeIn();
+    });
+
+    $(".btn-close, .menu-bg").click(function(){
+        $(".menu").toggleClass("is-active");
+        $( ".menu-bg" ).fadeOut();
+    });
+
+    $(".btn-next, .btn-next__container, .nav-links").on("click","a", function (event) {
+        event.preventDefault();
+        const id  = $(this).attr('href');
+            const {top} = $(id).offset();
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});

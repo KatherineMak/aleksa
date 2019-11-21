a06f9fae92c3082d74b2dc6d4d67daa0b4727952
KatherineMak <30404996+KatherineMak@users.noreply.github.com>
2019-11-21 12:42:36 +0200

    pop-up and sliders

--- src/js/import/swiper.js
+++ src/js/import/swiper.js
@@ -1,13 +1,31 @@
 import Swiper from 'swiper';
 
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 10,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
 });
+
+const swiper1 = new Swiper('.swiper-container-1', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 4,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    }
+});
+
 
 // //home
 // var swiper = new Swiper('.swiper-shares', {

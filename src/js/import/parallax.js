a50f5f90fa570c030911b881931db7103eb5d7cc
KatherineMak <30404996+KatherineMak@users.noreply.github.com>
2019-11-26 17:13:40 +0200

    fix

--- src/js/import/parallax.js
+++ src/js/import/parallax.js
@@ -1,6 +1,8 @@
 import Parallax from 'parallax-js';
+if(window.innerWidth > '1040') {
+  let spray = $('.intro__img-container').get(0);
+  let parallaxSpray = new Parallax(spray);
+  let brandName = $('.brand-name__container').get(0);
+  let parallaxBrandName = new Parallax(brandName);
+}
 
-let spray = $('.intro__img-container').get(0);
-let parallaxSpray = new Parallax(spray);
-let brandName = $('.brand-name__container').get(0);
-let parallaxBrandName = new Parallax(brandName);

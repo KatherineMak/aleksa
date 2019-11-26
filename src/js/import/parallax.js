11b306dd28c70a34f61bab415a47a29b42bcd04c
KatherineMak <30404996+KatherineMak@users.noreply.github.com>
2019-11-26 15:05:46 +0200

    test

--- src/js/import/parallax.js
+++ src/js/import/parallax.js
@@ -1,5 +1,4 @@
 import Parallax from 'parallax-js';
 
-let scene = $('.home-intro__img-container').get(0);
-// console.log(scene);
+let scene = $('.home-intro__content').get(0);
 let parallaxInstance = new Parallax(scene);

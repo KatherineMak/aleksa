22bd1e81b84c686fbfc5b4252a8091ecdbbee24a
Tim <ktn_96@icloud.com>
2019-11-28 15:48:44 +0200

    fix from tim

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 import './import/swiper';
 import './import/ui';
-import './import/parallax';
 import './import/jquery.Storage';
 import './import/ntsaveforms';
+import './import/parallax';
 // import objectFitImages from 'object-fit-images';
 //
 // objectFitImages();

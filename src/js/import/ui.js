7e90de4d53811a63eb5e9d175aaef5c78b9cf9d9
Tim <ktn_96@icloud.com>
2019-12-17 17:14:37 +0200

    fix

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -20,7 +20,7 @@ $('[rel="modal:open"]').on('click', function(event) {
 //fixed header
 window.onscroll = function showHeaderFixed() {
   const header = $('.header').get(0);
-  if(window.pageYOffset > 70) {
+  if(window.pageYOffset > 110) {
     header.classList.add('header-fixed');
   } else {
     header.classList.remove('header-fixed');
@@ -54,4 +54,16 @@ $('.btn-next, .btn-next__container, .nav-links, .home-advantages, .menu li').on(
   const id  = $(this).attr('href');
   const {top} = $(id).offset();
   $('body,html').animate({scrollTop: top - 50}, 1000);
+});
+
+
+//shop pop-up
+
+$('.product-description .open').on('click', function(event) {
+  event.preventDefault();
+  $(this).closest('.product-description').find('.product-description__pop-up').fadeIn();
+});
+$('.product-description__pop-up h3 a').on('click', function(event) {
+  event.preventDefault();
+  $(this).closest('.product-description__pop-up').fadeOut();
 });
